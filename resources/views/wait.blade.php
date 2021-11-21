a524fd2fbb7072b5cb85b13241a3fd768c80b5f2
Noan <noan.perrot@gmail.com>
2021-11-21 12:47:49 +0100

    change logo

--- resources/views/wait.blade.php
+++ resources/views/wait.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>22/11/2021 00H00</title>
+    <link rel="icon" type="image/png" href="{{asset('img/logo.png')}}">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
 
